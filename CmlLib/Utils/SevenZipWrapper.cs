3e949531b0f0fa4104c76f800852bd979181de73
ksi123456ab <ksi123456ab@naver.com>
2018-10-06 19:51:37 +0900

    add 7zip SDK , clean project

---
+++ CmlLib/Utils/SevenZipWrapper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CmlLib.Utils
+{
+    class SevenZipWrapper
+    {
+        public static void CompressFileLZMA(string inFile, string outFile)
+        {
+            SevenZip.Compression.LZMA.Encoder coder = new SevenZip.Compression.LZMA.Encoder();
+            FileStream input = new FileStream(inFile, FileMode.Open);
+            FileStream output = new FileStream(outFile, FileMode.Create);
+
+            // Write the encoder properties
+            coder.WriteCoderProperties(output);
+
+            // Write the decompressed file size.
+            output.Write(BitConverter.GetBytes(input.Length), 0, 8);
+
+            // Encode the file.
+            coder.Code(input, output, input.Length, -1, null);
+            output.Flush();
+            output.Close();
+        }
+
+        public static void DecompressFileLZMA(string inFile, string outFile)
+        {
+            SevenZip.Compression.LZMA.Decoder coder = new SevenZip.Compression.LZMA.Decoder();
+            FileStream input = new FileStream(inFile, FileMode.Open);
+            FileStream output = new FileStream(outFile, FileMode.Create);
+
+            // Read the decoder properties
+            byte[] properties = new byte[5];
+            input.Read(properties, 0, 5);
+
+            // Read in the decompress file size.
+            byte[] fileLengthBytes = new byte[8];
+            input.Read(fileLengthBytes, 0, 8);
+            long fileLength = BitConverter.ToInt64(fileLengthBytes, 0);
+
+            coder.SetDecoderProperties(properties);
+            coder.Code(input, output, input.Length, fileLength, null);
+            output.Flush();
+            output.Close();
+        }
+    }
+}
