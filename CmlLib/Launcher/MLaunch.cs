e8a1f95c6e6eddcbdc2eb5566dc9f263fee8b36d
AlphaBs <ksi123456ab@naver.com>
2019-11-23 22:53:58 +0900

    change file name

---
+++ CmlLib/Launcher/MLaunch.cs
@@ -0,0 +1,195 @@
+﻿using Ionic.Zip;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace CmlLib.Launcher
+{
+    /// <summary>
+    /// 게임 실행에 필요한 파라미터 생성, 게임 실행을 합니다.
+    /// </summary>
+    public class MLaunch
+    {
+        /// <summary>
+        /// 기본 자바 파라미터
+        /// </summary>
+        public static string DefaultJavaParameter =
+                "-XX:+UnlockExperimentalVMOptions " +
+                "-XX:+UseG1GC " +
+                "-XX:G1NewSizePercent=20 " +
+                "-XX:G1ReservePercent=20 " +
+                "-XX:MaxGCPauseMillis=50 " +
+                "-XX:G1HeapRegionSize=16M";
+        public static string SupportLaunchVersion = "1.3";
+
+        public MLaunch(MLaunchOption option)
+        {
+            option.CheckValid();
+            LaunchOption = option;
+        }
+
+        public MLaunchOption LaunchOption { get; private set; }
+
+        /// <summary>
+        /// 설정한 프로퍼티로 인수를 만들고 게임을 실행합니다.
+        /// </summary>
+        /// <param name="isdebug">true 로 설정하면 디버그 모드가 됩니다.</param>
+        public void Start(bool isdebug = false)
+        {
+            GetProcess(isdebug).Start();
+        }
+
+        /// <summary>
+        /// 설정한 프로퍼티로 인수를 만들어 Process 객체를 만들고 반환합니다.
+        /// </summary>
+        /// <param name="isdebug">true 로 설정하면 디버그 모드가 됩니다.</param>
+        /// <returns>만들어진 Process 객체</returns>
+        public Process GetProcess(bool isdebug = false)
+        {
+            var native = new MNative(LaunchOption);
+            native.CleanNatives();
+            native.CreateNatives();
+
+            string arg = makeArg();
+            Process mc = new Process();
+            mc.StartInfo.FileName = LaunchOption.JavaPath;
+            mc.StartInfo.Arguments = arg;
+            mc.StartInfo.WorkingDirectory = Minecraft.path;
+
+            if (isdebug)
+                File.WriteAllText("mc_arg.txt", mc.StartInfo.Arguments);
+
+            return mc;
+        }
+
+        string makeArg()
+        {
+            var profile = LaunchOption.StartProfile;
+
+            var sb = new StringBuilder();
+
+            ///// JAVA ARG /////
+
+            if (LaunchOption.CustomJavaParameter == "")
+                sb.Append(DefaultJavaParameter);
+            else
+                sb.Append(LaunchOption.CustomJavaParameter);
+
+            sb.Append(" -Xmx" + LaunchOption.MaximumRamMb + "m");
+            sb.Append(" -Djava.library.path=" + handleEmpty(LaunchOption.StartProfile.NativePath));
+            sb.Append(" -cp ");
+
+            foreach (var item in profile.Libraries)
+            {
+                if (!item.IsNative)
+                    sb.Append(handleEmpty(item.Path.Replace("/", "\\")) + ";");
+            }
+
+            ///// JAVA ARG END /////
+
+            string mcjarid = profile.Jar;
+
+            sb.Append(handleEmpty(Minecraft.Versions + mcjarid + "\\" + mcjarid + ".jar") + " ");
+            sb.Append(LaunchOption.StartProfile.MainClass + "  ");
+
+            ///// GAME ARG ///// 
+
+            Dictionary<string, string> argDicts = new Dictionary<string, string>()
+            {
+                { "${auth_player_name}", LaunchOption.Session.Username },
+                { "${version_name}", LaunchOption.StartProfile.Id },
+                { "${game_directory}", Minecraft._Path },
+                { "${assets_root}", Minecraft._Assets },
+                { "${assets_index_name}", profile.AssetId },
+                { "${auth_uuid}", LaunchOption.Session.UUID },
+                { "${auth_access_token}", LaunchOption.Session.AccessToken },
+                { "${user_properties}", "{}" },
+                { "${user_type}", "Mojang" },
+                { "${game_assets}", Minecraft.AssetLegacy },
+                { "${auth_session}", LaunchOption.Session.AccessToken }
+            };
+
+            if (LaunchOption.LauncherName == "")
+                argDicts.Add("${version_type}", profile.TypeStr);
+            else
+                argDicts.Add("${version_type}", LaunchOption.LauncherName);
+
+            if (LaunchOption.StartProfile.GameArguments != null) // Arguments Json 을 사용하는 1.3 과 그 이후의 버전
+            {
+                foreach (var item in LaunchOption.StartProfile.GameArguments)
+                {
+                    var argStr = item.ToString();
+
+                    if (argStr[0] != '$')
+                        sb.Append(argStr);
+                    else
+                    {
+                        var argValue = "";
+                        if (argDicts.TryGetValue(argStr, out argValue))
+                            sb.Append(handleEmpty(argValue));
+                        else
+                            sb.Append(argStr);
+                    }
+
+                    sb.Append(" ");
+                }
+            }
+            else // MinecraftArguments 를 사용하는 1.3 이전의 버전
+            {
+                var gameArgBuilder = new StringBuilder(LaunchOption.StartProfile.MinecraftArguments);
+
+                foreach (var item in argDicts)
+                {
+                    gameArgBuilder.Replace(item.Key, handleEmpty(item.Value));
+                }
+
+                sb.Append(gameArgBuilder.ToString());
+            }
+
+            // 추가 옵션들 설정
+
+            if (LaunchOption.ServerIp != "")
+                sb.Append(" --server " + LaunchOption.ServerIp);
+
+            if (LaunchOption.ScreenWidth > 0 && LaunchOption.ScreenHeight > 0)
+            {
+                sb.Append(" --width ");
+                sb.Append(LaunchOption.ScreenWidth);
+                sb.Append(" --height ");
+                sb.Append(LaunchOption.ScreenHeight);
+            }
+
+            return sb.ToString();
+        }
+
+        private void DeleteDirectory(string target_dir)
+        {
+            string[] files = Directory.GetFiles(target_dir);
+            string[] dirs = Directory.GetDirectories(target_dir);
+
+            foreach (string file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            foreach (string dir in dirs)
+            {
+                DeleteDirectory(dir);
+            }
+
+            Directory.Delete(target_dir, true);
+        }
+
+        private string handleEmpty(string input)
+        {
+            if (input.Contains(" "))
+                return "\"" + input + "\"";
+            else
+                return input;
+        }
+    }
+}
---
+++ CmlLib/Launcher/MLogin.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+
+namespace CmlLib.Launcher
+{
+    /// <summary>
+    /// 로그인 결과를 알려줍니다. 
+    /// </summary>
+    public enum MLoginResult { Success, BadRequest, WrongAccount, NeedLogin, UnknownError }
+
+    /// <summary>
+    /// 닉네임, 엑세스토큰, UUID 가 저장된 세션 클래스입니다.
+    /// </summary>
+    public class MSession
+    {
+        public string Username { get; internal set; }
+        public string AccessToken { get; internal set; }
+        public string UUID { get; internal set; }
+        public string ClientToken { get; internal set; }
+
+        public MLoginResult Result { get; internal set; }
+        public string Message { get; internal set; }
+
+        public string _RawResponse { get; internal set; }
+
+        public static MSession GetOfflineSession(string username)
+        {
+            var login = new MSession();
+            login.Username = username;
+            login.AccessToken = "access_token";
+            login.UUID = "user_uuid";
+            login.Result = MLoginResult.Success;
+            login.Message = "";
+            login.ClientToken = "";
+            return login;
+        }
+
+        internal static MSession createEmpty()
+        {
+            var session = new MSession();
+            session.Username = "";
+            session.AccessToken = "";
+            session.UUID = "";
+            session.ClientToken = "";
+            return session;
+        }
+    }
+
+    public class MLogin
+    {
+
+        public static readonly string DefaultLoginSessionFile = Minecraft.DefaultPath + "\\logintoken.json";
+
+        public MLogin() : this(DefaultLoginSessionFile) { }
+
+        public MLogin(string tokenpath)
+        {
+            TokenFile = tokenpath;
+        }
+
+        public string TokenFile;
+        public bool SaveSession = true;
+
+        private void WriteLogin(MSession result)
+        {
+            WriteLogin(result.Username, result.AccessToken, result.UUID, result.ClientToken);
+        }
+
+        // Save Login Session
+        private void WriteLogin(string us, string se, string id, string ct)
+        {
+            if (!SaveSession) return;
+
+            JObject jobj = new JObject(); // create session json
+            jobj.Add("username", us);
+            jobj.Add("session", se);
+            jobj.Add("uuid", id);
+            jobj.Add("clientToken", ct);
+
+            Directory.CreateDirectory(Path.GetDirectoryName(TokenFile));
+
+            File.WriteAllText(TokenFile,jobj.ToString() , Encoding.UTF8);
+        }
+
+        public MSession GetLocalToken()
+        {
+            MSession session;
+
+            if (!File.Exists(TokenFile)) // no session data
+            {
+                var ClientToken = Guid.NewGuid().ToString().Replace("-", ""); // create new clienttoken
+
+                session = MSession.createEmpty();
+                session.ClientToken = ClientToken;
+
+                WriteLogin(session);
+            }
+            else // exists session data
+            {
+                var filedata = File.ReadAllText(TokenFile, Encoding.UTF8);
+                try
+                {
+                    var job = JObject.Parse(filedata);
+                    session = new MSession();
+                    session.AccessToken = job["session"]?.ToString();
+                    session.UUID = job["uuid"]?.ToString();
+                    session.Username = job["username"]?.ToString();
+                    session.ClientToken = job["clientToken"]?.ToString();
+                }
+                catch (Newtonsoft.Json.JsonReaderException) // if JSON file isn't vaild
+                {
+                    DeleteTokenFile();
+                    session = GetLocalToken();
+                }
+            }
+
+            return session;
+        }
+
+        private HttpWebResponse mojangRequest(string endpoint, string postdata)
+        {
+            var http = WebRequest.CreateHttp("https://authserver.mojang.com/" + endpoint);
+            http.ContentType = "application/json";
+            http.Method = "POST";
+            using (var req = new StreamWriter(http.GetRequestStream()))
+            {
+                req.Write(postdata);
+                req.Flush();
+            }
+
+            var res = http.GetResponseNoException();
+            return res;
+        }
+
+        public MSession Authenticate(string id, string pw)
+        {
+            MSession result = new MSession();
+
+            string ClientToken = GetLocalToken().ClientToken;
+
+            var job = new JObject
+            {
+                { "username", id },
+                { "password", pw },
+                { "clientToken", ClientToken },
+
+                { "agent", new JObject
+                    {
+                        { "name", "Minecraft" },
+                        { "version", 1 }
+                    }
+                }
+            };
+
+            var resHeader = mojangRequest("authenticate", job.ToString());
+
+            using (var res = new StreamReader(resHeader.GetResponseStream()))
+            {
+                var Response = res.ReadToEnd();
+                
+                result.ClientToken = ClientToken;
+
+                if (resHeader.StatusCode == HttpStatusCode.OK) // ResultCode == 200
+                {
+                    var jObj = JObject.Parse(Response); //json parse
+                    result.AccessToken = jObj["accessToken"].ToString();
+                    result.UUID = jObj["selectedProfile"]["id"].ToString();
+                    result.Username = jObj["selectedProfile"]["name"].ToString();
+
+                    WriteLogin(result); //로그인 정보 쓰기
+                    result.Result = MLoginResult.Success;
+                }
+                else //로그인이 실패하였을때
+                {
+                    var json = JObject.Parse(Response); 
+
+                    var error = json["error"]?.ToString(); // error type
+                    result._RawResponse = Response;
+                    result.Message = json["message"]?.ToString() ?? ""; // detail error message
+
+                    switch (error)
+                    {
+                        case "Method Not Allowed":
+                        case "Not Found":
+                        case "Unsupported Media Type":
+                            result.Result = MLoginResult.BadRequest;
+                            break;
+                        case "IllegalArgumentException":
+                        case "ForbiddenOperationException":
+                            result.Result = MLoginResult.WrongAccount;
+                            break;
+                        default:
+                            result.Result = MLoginResult.UnknownError;
+                            break;
+                    }
+                }
+
+                return result;
+            }
+        }
+
+        public MSession TryAutoLogin()
+        {
+            MSession result;
+            result = Validate();
+            if (result.Result != MLoginResult.Success)
+                result = Refresh();
+
+            return result;
+        }
+
+        public MSession Refresh() //엑세스 토큰 새로가져오기
+        {
+            var result = new MSession();
+
+            try
+            {
+                var session = GetLocalToken();
+
+                var req = new JObject
+                {
+                    { "accessToken", session.AccessToken },
+                    { "clientToken", session.ClientToken },
+                    { "selectedProfile", new JObject()
+                        {
+                            { "id", session.UUID },
+                            { "name", session.Username }
+                        }
+                    }
+                };
+
+                var resHeader = mojangRequest("refresh", req.ToString());
+                using (var res = new StreamReader(resHeader.GetResponseStream()))
+                {
+                    var response = res.ReadToEnd();
+                    result._RawResponse = response;
+                    JObject job = JObject.Parse(response); //json 파일 파싱
+
+                    result.AccessToken = job["accessToken"].ToString();
+                    result.AccessToken = job["accessToken"].ToString();
+                    result.UUID = job["selectedProfile"]["id"].ToString();
+                    result.Username = job["selectedProfile"]["name"].ToString();
+                    result.ClientToken = session.ClientToken;
+
+                    WriteLogin(result); //로그인 정보 쓰기
+                    result.Result = MLoginResult.Success;
+                }
+            }
+            catch
+            {
+                result.Result = MLoginResult.UnknownError;
+            }
+
+            return result;
+        }
+
+        public MSession Validate() //현재 토큰이 유효한 토큰인지 검사
+        {
+            //(오래된 토큰은 사용불가능하므로 RefreshLogin 메서드로 새로가져와야함)
+
+            var result = new MSession();
+            try
+            {
+                var session = GetLocalToken();
+
+                JObject job = new JObject
+                {
+                    { "accessToken", session.AccessToken },
+                    { "clientToken", session.ClientToken }
+                };
+
+                var resHeader = mojangRequest("validate", job.ToString());
+                using (var res = new StreamReader(resHeader.GetResponseStream()))
+                {
+                    if (resHeader.StatusCode == HttpStatusCode.NoContent) // StatusCode == 204
+                    {
+                        result.Result = MLoginResult.Success;
+                        result.AccessToken = session.AccessToken;
+                        result.UUID = session.UUID;
+                        result.Username = session.Username;
+                        result.ClientToken = session.ClientToken;
+                    }
+                    else
+                        result.Result = MLoginResult.NeedLogin;
+                }
+            }
+            catch
+            {
+                result.Result = MLoginResult.UnknownError;
+            }
+
+            return result;
+        }
+
+        public void DeleteTokenFile()
+        {
+            if (File.Exists(TokenFile))
+                File.Delete(TokenFile);
+        }
+
+        public bool Invalidate()
+        {
+            var session = GetLocalToken();
+
+            var job = new JObject
+            {
+                { "accessToken", session.AccessToken },
+                { "clientToken", session.ClientToken }
+            };
+
+            var res = mojangRequest("invalidate", job.ToString());
+            return res.StatusCode == HttpStatusCode.OK; // 200
+        }
+
+        public bool Signout(string id, string pw)
+        {
+            var job = new JObject
+            {
+                { "username", id },
+                { "password", pw }
+            };
+
+            var res = mojangRequest("signout", job.ToString());
+            return res.StatusCode == HttpStatusCode.NoContent; // 204
+        }
+    }
+
+    public static class HttpWebResponseExt
+    {
+        public static HttpWebResponse GetResponseNoException(this HttpWebRequest req)
+        {
+            try
+            {
+                return (HttpWebResponse)req.GetResponse();
+            }
+            catch (WebException we)
+            {
+                var resp = we.Response as HttpWebResponse;
+                if (resp == null)
+                    throw;
+                return resp;
+            }
+        }
+    }
+}
