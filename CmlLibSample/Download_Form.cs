c12c04f18fd98edea0e01df3b2f8eb6a34cff572
M1XT3NZ <glpfan90@gmail.com>
2019-11-26 03:24:21 +0100

    Readabillity
    Just did a little makeover of the most button and form names.
    To make it a lot more readable and understandable if you want to Read the sample

---
+++ CmlLibSample/Download_Form.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+using CmlLib.Launcher;
+
+namespace CmlLibSample
+{
+    public partial class Download_Form : Form
+    {
+        public void ChangeProgress(int p)
+        {
+            try
+            {
+                progressBar1.Value = p;
+            }
+            catch
+            {
+
+            }
+        }
+
+        public Download_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
