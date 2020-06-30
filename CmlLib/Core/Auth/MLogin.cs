ed3fede4afdb6a855316baea9e9a07c0034f8552
AlphaBs <ksi123456ab@naver.com>
2020-06-30 23:40:46 +0900

    seperate MLoginResponse from MLogin

--- CmlLib/Core/Auth/MLogin.cs
+++ CmlLib/Core/Auth/MLogin.cs
@@ -11,24 +11,6 @@ namespace CmlLib.Core.Auth
 
     public class MLogin
     {
-        public class MLoginResponse
-        {
-            public MLoginResponse(MLoginResult result, MSession session, string errormsg, string rawresponse)
-            {
-                Result = result;
-                Session = session;
-                ErrorMessage = errormsg;
-                RawResponse = rawresponse;
-            }
-
-            public MLoginResult Result { get; private set; }
-            public MSession Session { get; private set; }
-            public string ErrorMessage { get; private set; }
-            public string RawResponse { get; private set; }
-
-            public bool IsSuccess => Result == MLoginResult.Success;
-        }
-
         public static readonly string DefaultLoginSessionFile = Path.Combine(Minecraft.GetOSDefaultPath(), "logintoken.json");
 
         public MLogin() : this(DefaultLoginSessionFile) { }
