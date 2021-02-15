0c4d9d300d92dcfcb77aacd1c15dbc985876dbe5
ksi123456ab <ksi123456ab@naver.com>
2021-02-16 01:39:12 +0900

    add Mod downloading system

---
+++ CmlLib/Core/Files/ModChecker.cs
@@ -0,0 +1,60 @@
+﻿using CmlLib.Core.Downloader;
+using CmlLib.Core.Version;
+using CmlLib.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CmlLib.Core.Files
+{
+    public class ModChecker : IFileChecker
+    {
+        public event DownloadFileChangedHandler ChangeFile;
+
+        public bool CheckHash { get; set; } = true;
+        public ModFile[] Mods;
+
+        public DownloadFile[] CheckFiles(MinecraftPath path, MVersion version)
+        {
+            if (version == null)
+                throw new ArgumentNullException(nameof(version));
+
+            return Mods
+                .Where(mod => CheckDownloadRequire(path, mod))
+                .Select(mod => new DownloadFile
+                {
+                    Type = MFile.Others,
+                    Name = mod.Name,
+                    Path = Path.Combine(path.BasePath, mod.Path),
+                    Url = mod.Url
+                })
+                .Distinct()
+                .ToArray();
+        }
+
+        private bool CheckDownloadRequire(MinecraftPath path, ModFile mod)
+        {
+            return !string.IsNullOrEmpty(mod.Url)
+                && !string.IsNullOrEmpty(mod.Path)
+                && !CheckFileValidation(Path.Combine(path.BasePath, mod.Path), mod.Hash);
+        }
+
+        private bool CheckFileValidation(string path, string hash)
+        {
+            if (!File.Exists(path))
+                return false;
+
+            if (!CheckHash)
+                return true;
+            else
+                return IOUtil.CheckFileValidation(path, hash);
+        }
+
+        public void fire(MFile type, string name, int total, int progressed)
+        {
+            ChangeFile?.Invoke(new DownloadFileChangedEventArgs(type, name, total, progressed));
+        }
+    }
+}
---
+++ CmlLib/Core/Files/ModFileFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CmlLib.Core.Files
+{
+    public class ModFileFactory
+    {
+        public ModFile GetCurseForgeModFile(string modname, string fileid)
+        {
+            return new ModFile
+            {
+                Name = modname,
+                Path = Path.Combine("mods", modname + ".jar"),
+                Url = $"https://www.curseforge.com/minecraft/mc-mods/{modname}/download/{fileid}/file"
+            };
+        }
+
+        public ModFile GetCurseForgeModFile(string modname, string fileid, string filehash)
+        {
+            ModFile mod = GetCurseForgeModFile(modname, fileid);
+            mod.Hash = filehash;
+            return mod;
+        }
+    }
+}
