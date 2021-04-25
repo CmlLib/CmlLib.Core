6d03e1c09e9f636a78457ecbbf6315e2cdfa5d8a
AlphaBs <ksi123456ab@naver.com>
2021-04-25 12:43:57 +0900

    add async java downloader

--- CmlLib/Core/Installer/MJava.cs
+++ CmlLib/Core/Installer/MJava.cs
@@ -4,6 +4,7 @@ using System;
 using System.ComponentModel;
 using System.IO;
 using System.Net;
+using System.Threading.Tasks;
 
 namespace CmlLib.Core.Installer
 {
@@ -15,6 +16,8 @@ namespace CmlLib.Core.Installer
         public event EventHandler DownloadCompleted;
         public string RuntimeDirectory { get; private set; }
 
+        IProgress<ProgressChangedEventArgs> pProgressChanged;
+
         public MJava() : this(DefaultRuntimeDirectory) { }
 
         public MJava(string runtimePath)
@@ -22,51 +25,39 @@ namespace CmlLib.Core.Installer
             RuntimeDirectory = runtimePath;
         }
 
-        public string CheckJava()
+        public string GetDefaultBinaryName()
         {
             var binaryName = "java";
             if (MRule.OSName == MRule.Windows)
                 binaryName = "javaw.exe";
+            return binaryName;
+        }
 
+        public string CheckJava()
+        {
+            var binaryName = GetDefaultBinaryName();
             return CheckJava(binaryName);
+        }
+
+        public async Task<string> CheckJavaAsync()
+        {
+            var binaryName = GetDefaultBinaryName();
+            return await CheckJavaAsync(binaryName);
         }
 
         public string CheckJava(string binaryName)
         {
+            pProgressChanged = new Progress<ProgressChangedEventArgs>(
+                (e) => ProgressChanged?.Invoke(this, e));
+
             var javapath = Path.Combine(RuntimeDirectory, "bin", binaryName);
 
             if (!File.Exists(javapath))
             {
-                string json = "";
+                var javaUrl = GetJavaUrl();
+                var lzmaPath = DownloadJavaLzma(javaUrl);
 
-                var javaUrl = "";
-                using (var wc = new WebClient())
-                {
-                    json = wc.DownloadString(MojangServer.LauncherMeta);
-
-                    var job = JObject.Parse(json)[MRule.OSName];
-                    javaUrl = job[MRule.Arch]?["jre"]?["url"]?.ToString();
-
-                    if (string.IsNullOrEmpty(javaUrl))
-                        throw new PlatformNotSupportedException("Downloading JRE on current OS is not supported. Set JavaPath manually.");
-
-                    Directory.CreateDirectory(RuntimeDirectory);
-                }
-
-                var lzmapath = Path.Combine(Path.GetTempPath(), "jre.lzma");
-                var zippath = Path.Combine(Path.GetTempPath(), "jre.zip");
-
-                var webdownloader = new WebDownload();
-                webdownloader.DownloadProgressChangedEvent += Downloader_DownloadProgressChangedEvent;
-                webdownloader.DownloadFile(javaUrl, lzmapath);
-
-                DownloadCompleted?.Invoke(this, new EventArgs());
-
-                SevenZipWrapper.DecompressFileLZMA(lzmapath, zippath);
-
-                var z = new SharpZip(zippath);
-                z.ProgressEvent += Z_ProgressEvent;
-                z.Unzip(RuntimeDirectory);
+                DecompressJavaFile(lzmaPath);
 
                 if (!File.Exists(javapath))
                     throw new Exception("Failed Download");
@@ -78,19 +69,106 @@ namespace CmlLib.Core.Installer
             return javapath;
         }
 
-        private void Z_ProgressEvent(object sender, int e)
+        public async Task<string> CheckJavaAsync(string binaryName)
         {
-            ProgressChanged?.Invoke(this, new ProgressChangedEventArgs(e, null));
+            pProgressChanged = new Progress<ProgressChangedEventArgs>(
+    (e) => ProgressChanged?.Invoke(this, e));
+
+            var javapath = Path.Combine(RuntimeDirectory, "bin", binaryName);
+
+            if (!File.Exists(javapath))
+            {
+                var javaUrl = await GetJavaUrlAsync();
+                var lzmaPath = await DownloadJavaLzmaAsync(javaUrl);
+
+                var decompressTask = Task.Run(() => DecompressJavaFile(lzmaPath));
+                await decompressTask;
+
+                if (!File.Exists(javapath))
+                    throw new Exception("Failed Download");
+
+                if (MRule.OSName != MRule.Windows)
+                    IOUtil.Chmod(javapath, IOUtil.Chmod755);
+            }
+
+            return javapath;
         }
 
-        private void Szip_ProgressChange(object sender, ProgressChangedEventArgs e)
+        private string GetJavaUrl()
         {
-            ProgressChanged?.Invoke(this, e);
+            using (var wc = new WebClient())
+            {
+                var json = wc.DownloadString(MojangServer.LauncherMeta);
+                return parseLauncherMetadata(json);
+            }
+        }
+
+        private async Task<string> GetJavaUrlAsync()
+        {
+            using (var wc = new WebClient())
+            {
+                var json = await wc.DownloadStringTaskAsync(MojangServer.LauncherMeta);
+                return parseLauncherMetadata(json);
+            }
+        }
+
+        private string parseLauncherMetadata(string json)
+        {
+            var job = JObject.Parse(json)[MRule.OSName];
+            var javaUrl = job[MRule.Arch]?["jre"]?["url"]?.ToString();
+
+            if (string.IsNullOrEmpty(javaUrl))
+                throw new PlatformNotSupportedException("Downloading JRE on current OS is not supported. Set JavaPath manually.");
+            return javaUrl;
+        }
+
+        private string DownloadJavaLzma(string javaUrl)
+        {
+            Directory.CreateDirectory(RuntimeDirectory);
+            var lzmapath = Path.Combine(Path.GetTempPath(), "jre.lzma");
+
+            var webdownloader = new WebDownload();
+            webdownloader.DownloadProgressChangedEvent += Downloader_DownloadProgressChangedEvent;
+            webdownloader.DownloadFile(javaUrl, lzmapath);
+            DownloadCompleted?.Invoke(this, new EventArgs());
+
+            return lzmapath;
+        }
+
+        private async Task<string> DownloadJavaLzmaAsync(string javaUrl)
+        {
+            Directory.CreateDirectory(RuntimeDirectory);
+            var lzmapath = Path.Combine(Path.GetTempPath(), "jre.lzma");
+
+            using (var wc = new WebClient())
+            {
+                wc.DownloadProgressChanged += Downloader_DownloadProgressChangedEvent;
+                await wc.DownloadFileTaskAsync(javaUrl, lzmapath);
+            }
+
+            DownloadCompleted?.Invoke(this, new EventArgs());
+
+            return lzmapath;
+        }
+
+        private void DecompressJavaFile(string lzmaPath)
+        {
+            var zippath = Path.Combine(Path.GetTempPath(), "jre.zip");
+            SevenZipWrapper.DecompressFileLZMA(lzmaPath, zippath);
+
+            var z = new SharpZip(zippath);
+            z.ProgressEvent += Z_ProgressEvent;
+            z.Unzip(RuntimeDirectory);
+        }
+
+        private void Z_ProgressEvent(object sender, int e)
+        {
+            pProgressChanged.Report(new ProgressChangedEventArgs(50 + e / 2, null));
         }
 
         private void Downloader_DownloadProgressChangedEvent(object sender, ProgressChangedEventArgs e)
         {
-            ProgressChanged?.Invoke(this, e);
+            ProgressChanged?.Invoke(this, new ProgressChangedEventArgs(e.ProgressPercentage / 2, null));
         }
     }
 }
