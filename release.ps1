2e20905037130fcd7c7e12a4241a9ac3391019d0
Liam Sho <conan_creeper@outlook.com>
2021-09-22 23:37:09 +0800

    Update release powershell script

--- release.ps1
+++ release.ps1
@@ -1,9 +1,7 @@
-if ($args.Length -gt 0)
-{
-    $ver=$args[0]
+if ($args.Length -gt 0) {
+    $ver = $args[0]
 }
-else
-{
+else {
     $ver = Read-Host 'version'
 }
 
@@ -14,9 +12,8 @@ mkdir .\release\SampleCoreLauncher$ver
 mkdir .\release\SampleWinformLauncher$ver
 
 Copy-Item .\CmlLib\bin\Release\* -Destination .\release\CmlLib.Core.$ver -Recurse
-Get-ChildItem .\release\CmlLib.Core.$ver -Recurse -File | Where {($_.Extension -ne ".dll")} | Remove-Item
-Copy-Item -Path .\CmlLibCoreSample\bin\Release\netcoreapp3.1\* -Destination .\release\SampleCoreLauncher$ver
-Copy-Item .\CmlLibWinFormSample\bin\Release\* -Destination .\release\SampleWinformLauncher$ver -Include *.exe,*.dll,*.pdb,*.config
+Get-ChildItem .\release\CmlLib.Core.$ver -Recurse -File | Where-Object { ($_.Extension -ne ".dll") } | Remove-Item
+Copy-Item .\CmlLibWinFormSample\bin\Release\* -Destination .\release\SampleWinformLauncher$ver -Include *.exe, *.dll, *.pdb, *.config
 
 Compress-Archive -Path .\release\CmlLib.Core.$ver -DestinationPath .\release\CmlLib.Core.$ver.zip
 Compress-Archive -Path .\release\SampleCoreLauncher$ver -DestinationPath .\release\SampleCoreLauncher$ver.zip
