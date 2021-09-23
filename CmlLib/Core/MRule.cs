e0ffdd76ef8d902af338034144ec32b6b7fdb430
AlphaBs <ksi123456ab@naver.com>
2021-09-23 09:17:49 +0900

    add netframework 4.6.2

--- CmlLib/Core/MRule.cs
+++ CmlLib/Core/MRule.cs
@@ -25,12 +25,24 @@ namespace CmlLib.Core
 
         private static string getOSName()
         {
+            // RuntimeInformation does not work in .NET Framework
+#if NETFRAMEWORK
+            var osType = Environment.OSVersion.Platform;
+
+            if (osType == PlatformID.MacOSX)
+                return OSX;
+            else if (osType == PlatformID.Unix)
+                return Linux;
+            else
+                return Windows;
+#else
             if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
                 return OSX;
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 return Windows;
             else
                 return Linux;
+#endif
         }
 
         public static bool CheckOSRequire(JArray arr)
