1cf3ff7dbdb5679df72ef4c7816fb89b9fb05f90
Hazar <hazar.akbuz@outlook.com>
2023-01-21 13:10:36 +0300

    Updated http->https protocol of resource page
    The page no longer supports http protocol.

--- CmlLib/Core/MojangServer.cs
+++ CmlLib/Core/MojangServer.cs
@@ -6,7 +6,7 @@
             Auth             = "https://authserver.mojang.com/",
             Version          = "https://launchermeta.mojang.com/mc/game/version_manifest.json",
             Library          = "https://libraries.minecraft.net/",
-            ResourceDownload = "http://resources.download.minecraft.net/",
+            ResourceDownload = "https://resources.download.minecraft.net/",
             LauncherMeta     = "http://launchermeta.mojang.com/mc/launcher.json",
             JavaManifest     = "https://launchermeta.mojang.com/v1/products/java-runtime/2ec0cc96c44e5a76b9c8b7c39df7210883d12871/all.json";
     }
