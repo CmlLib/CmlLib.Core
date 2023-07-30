39ebd98f6f892f7ad6b8b5ea32025c0cf339c87e
AlphaBs <ksi123456ab@naver.com>
2023-07-30 09:03:55 +0000

    migrate CmlLibWinFormSample to net6.0-windows

--- CmlLibWinFormSample/Program.cs
+++ CmlLibWinFormSample/Program.cs
@@ -11,7 +11,6 @@ namespace CmlLibWinFormSample
         [STAThread]
         static void Main()
         {
-            Console.WriteLine(CmlLib._Test.tstr); // for test
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginForm());
