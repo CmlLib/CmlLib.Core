966ef5e8b0806003d8d5b314acd4dae0d805497d
AlphaBs <ksi123456ab@naver.com>
2024-10-29 19:29:30 +0900

    feat: add more example code for LiteLoader and Fabric

--- examples/console/FabricTester.cs
+++ examples/console/FabricTester.cs
@@ -21,4 +21,35 @@ public class FabricTester
             await fabricInstaller.Install(version, _minecraftPath);
         }
     }
+
+    public async Task GetMinecraftVersions()
+    {
+        var fabricInstaller = new FabricInstaller(new HttpClient());
+        var versions = await fabricInstaller.GetSupportedVersionNames();
+
+        foreach (var version in versions)
+        {
+            Console.WriteLine(version);
+        }
+    }
+
+    public async Task GetFabricVersions()
+    {
+        var fabricInstaller = new FabricInstaller(new HttpClient());
+        var versions = await fabricInstaller.GetLoaders();
+
+        foreach (var version in versions)
+        {
+            Console.WriteLine(version.Version);
+        }
+    }
+
+    public async Task Install()
+    {
+        var path = new MinecraftPath();
+        var launcher = new MinecraftLauncher(path);
+
+        var fabricInstaller = new FabricInstaller(new HttpClient());
+        var versionName = await fabricInstaller.Install("1.20.4", "0.16.0", path);
+    }
 }
--- examples/console/LiteLoaderTester.cs
+++ examples/console/LiteLoaderTester.cs
@@ -24,4 +24,31 @@ public class LiteLoaderTester
             await liteLoaderInstaller.Install(loader, baseVersion, _launcher.MinecraftPath);
         }
     }
+
+    public async Task GetAllVersions()
+    {
+        var liteLoaderInstaller = new LiteLoaderInstaller(new HttpClient());
+        var loaders = await liteLoaderInstaller.GetAllLiteLoaders();
+
+        foreach (var loader in loaders)
+        {
+            Console.WriteLine($"GameVersion: {loader.BaseVersion}, LoaderVersion: {loader.Version}");
+        }
+    }
+
+    public async Task Install()
+    {
+        var path = new MinecraftPath();
+        var launcher = new MinecraftLauncher(path);
+        var version = "1.7.10";
+
+        var liteLoaderInstaller = new LiteLoaderInstaller(new HttpClient());
+        var loaders = await liteLoaderInstaller.GetAllLiteLoaders();
+        var loaderToInstall = loaders.First(loader => loader.BaseVersion == version);
+
+        var installedVersion = await liteLoaderInstaller.Install(
+            loaderToInstall, 
+            await launcher.GetVersionAsync(version), 
+            path);
+    }
 }
